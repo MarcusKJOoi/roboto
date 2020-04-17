e75eccf4a4df062a1b957f6905d593c3960e00af
Marcus <mooi0002@student.monash.edu>
2020-04-17 10:29:51 +1000

    Handle placing

--- index.js
+++ index.js
@@ -7,6 +7,20 @@ var Robot = /** @class */ (function () {
         this.y = y;
         this.facing = facing;
     }
+    Robot.prototype.move = function () {
+        if (this.facing === Direction.North) {
+            this.x;
+        }
+        if (this.facing === Direction.East) {
+        }
+        if (this.facing === Direction.South) {
+        }
+        if (this.facing === Direction.West) {
+        }
+    };
+    Robot.prototype.report = function () {
+        console.log(this.x + "," + this.y + "," + this.facing);
+    };
     return Robot;
 }());
 // If we wanted to add ordinal directions (NE/NW/SE/SW), we can fill it in easily
@@ -17,26 +31,31 @@ var Direction;
     Direction[Direction["South"] = 4] = "South";
     Direction[Direction["West"] = 6] = "West";
 })(Direction || (Direction = {}));
-var gridSize = 5; // 5 x 5 square grid
-function placeRobot() {
-}
+/*
+    0, 0 is the south west corner of the grid
+*/
+var gridSize = 5;
 function moveRobot() {
 }
 function rotateLeft() {
 }
 function rotateRight() {
 }
-function invalidInput() {
-    console.error('Invalid input was given!');
-}
 function reportPosition() {
     // console.log(`${},${},${}`);
 }
-function parsePrompt(input) {
+function parsePrompt(input, args) {
     var robot;
     switch (input) {
         case 'PLACE':
-            placeRobot();
+            // At the moment we assume that there can only be
+            // one robot on the field at any given time
+            if (robot === undefined) {
+                robot = new Robot(args[0], args[1], args[2]);
+            }
+            else {
+                console.error('An attempt was made to place multiple robots');
+            }
             break;
         case 'MOVE':
             moveRobot();
@@ -51,13 +70,41 @@ function parsePrompt(input) {
             reportPosition();
             break;
         default:
+            console.log('Unrecognized command provided', input);
             break;
     }
 }
+function directionToEnum(direction) {
+    if (direction === 'NORTH') {
+        return Direction.North;
+    }
+    if (direction === 'EAST') {
+        return Direction.East;
+    }
+    if (direction === 'SOUTH') {
+        return Direction.South;
+    }
+    if (direction === 'WEST') {
+        return Direction.West;
+    }
+    console.error('Invalid direction given as input', direction);
+    return -1;
+}
 fs.readFile('a.txt', 'utf8', function (error, data) {
     if (error) {
-        console.error(error);
+        console.error('Error reading file:', error);
     }
     var commands = data.split('\n');
-    console.log(commands);
+    commands.forEach(function (command) {
+        var split = command.split(' '); // Need to handle PLACE <x,y,d> command
+        var placeArguments = [];
+        if (split.length === 2) {
+            // This is a PLACE <x,y,d> command
+            var values = split[1].split(',');
+            placeArguments = values.map(function (argument) { return parseInt(argument, 10); });
+            // Convert facing direction to enum
+            placeArguments[2] = directionToEnum(values[2]);
+        }
+        parsePrompt(split[0], placeArguments);
+    });
 });
--- index.ts
+++ index.ts
@@ -10,6 +10,25 @@ class Robot {
         this.y = y;
         this.facing = facing;
     }
+
+    move() {
+        if (this.facing === Direction.North) {
+            this.x
+        }
+        if (this.facing === Direction.East) {
+
+        }
+        if (this.facing === Direction.South) {
+
+        }
+        if (this.facing === Direction.West) {
+
+        }
+    }
+
+    report() {
+        console.log(`${this.x},${this.y},${this.facing}`);
+    }
 }
 
 // If we wanted to add ordinal directions (NE/NW/SE/SW), we can fill it in easily
@@ -20,11 +39,10 @@ enum Direction {
     West = 6,
 } 
 
-const gridSize: Number = 5; // 5 x 5 square grid
-
-function placeRobot() {
-
-}
+/*
+    0, 0 is the south west corner of the grid
+*/
+const gridSize: Number = 5;
 
 function moveRobot() {
 
@@ -38,19 +56,21 @@ function rotateRight() {
 
 }
 
-function invalidInput() {
-    console.error('Invalid input was given!');
-} 
-
 function reportPosition() {
     // console.log(`${},${},${}`);
 }
 
-function parsePrompt(input: string) {
+function parsePrompt(input: string, args?: Array<number>) {
     let robot: Robot;
     switch(input) {
         case 'PLACE':
-            placeRobot();
+            // At the moment we assume that there can only be
+            // one robot on the field at any given time
+            if (robot === undefined) {
+                robot = new Robot(args[0], args[1], args[2]);
+            } else {
+                console.error('An attempt was made to place multiple robots');
+            }
             break;
         case 'MOVE':
             moveRobot();
@@ -65,14 +85,43 @@ function parsePrompt(input: string) {
             reportPosition();
             break;
         default:
+            console.log('Unrecognized command provided', input);
             break;
     }
 }
 
+function directionToEnum(direction: string): Direction | number {
+    if (direction === 'NORTH') {
+        return Direction.North;
+    }
+    if (direction === 'EAST') {
+        return Direction.East;
+    }
+    if (direction === 'SOUTH') {
+        return Direction.South;
+    }
+    if (direction === 'WEST') {
+        return Direction.West;
+    }
+    console.error('Invalid direction given as input', direction);
+    return -1;
+}
+
 fs.readFile('a.txt', 'utf8', (error, data) => {
     if (error) {
-        console.error(error);
+        console.error('Error reading file:', error);
     }
     const commands: Array<string> = data.split('\n');
-    console.log(commands);
+    commands.forEach(command => {
+        const split: Array<string> = command.split(' '); // Need to handle PLACE <x,y,d> command
+        let placeArguments: Array<number> = [];
+        if (split.length === 2) {
+            // This is a PLACE <x,y,d> command
+            const values = split[1].split(',');
+            placeArguments = values.map(argument => parseInt(argument, 10));
+            // Convert facing direction to enum
+            placeArguments[2] = directionToEnum(values[2]);
+        }
+        parsePrompt(split[0], placeArguments);
+    })
 });
